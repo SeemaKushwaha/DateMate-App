f7702b763b0f61a52a7b42253eb05641499eaa1b
seema kushwaha <kushwaha.seema14@live.com>
2020-04-20 17:24:19 +0530

    Initial Commit

---
+++ DateMate.API/Controllers/ValuesController.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DateMate.API.Data;
+
+namespace DateMate.API.Controllers
+{
+ [Route("api/[controller]")]
+ [ApiController]   
+public class ValuesController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public ValuesController(DataContext datacontext){
+        _context= datacontext;
+    }
+    [HttpGet]
+    public async Task<IActionResult> GetValues()
+    {
+        var values= await _context.Values.ToListAsync();
+        return Ok(values);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetValue(int id)
+    {
+        var value= await _context.Values.FirstOrDefaultAsync(x =>x.Id == id);
+        return Ok(value);
+    }
+
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+        
+    }
+
+    [HttpPut("{id}")]
+    public void Put(int id,[FromBody] string value)
+    {
+        
+    }
+
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+        
+    }
+}
+}
---
+++ DateMate.API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using DateMate.API.Models;
+namespace DateMate.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base (options){}
+        
+        public DbSet<Value> Values { get; set; }
+    }
+}
