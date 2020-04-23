0ba919a9955acc3400bf51977bb195e9cbfe9b11
seema kushwaha <kushwaha.seema14@live.com>
2020-04-23 23:55:52 +0530

    section 4

--- DateMate.API/Controllers/ValuesController.cs
+++ DateMate.API/Controllers/ValuesController.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace DateMate.API.Controllers
 {
- [Authorize]
+ [AllowAnonymous]
  [Route("api/[controller]")]
  [ApiController]   
 public class ValuesController : ControllerBase
