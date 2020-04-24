d8335f2aa12fd225bf663ce5929dd72139c9c4e8
seema kushwaha <kushwaha.seema14@live.com>
2020-04-24 18:59:21 +0530

    Section 6

--- DateMate-SPA/src/app/app.component.ts
+++ DateMate-SPA/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './_services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'DateMate App';
+  constructor(private authservice: AuthService) {
+
+  }
+  ngOnInit(): void {
+    const token = localStorage.getItem('token');
+    if (token)
+    {
+      this.authservice.decodedToken = this.authservice.jwtHelper.decodeToken(token);
+    }
+  }
 }
